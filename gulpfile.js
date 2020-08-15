2af0d694bad3d1cbbbb930538c80b44c7bce8ea9
AntonMyatto <48759290+AntonMyatto@users.noreply.github.com>
2020-08-15 21:18:33 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./styles/'));
+});
